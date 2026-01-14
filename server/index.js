7eab9a5baca39912ca28ab18d7018bba48259c29
AtharvBolke99 <atharvbolke99@gmail.com>
2026-01-14 22:18:00 +0530

    home and health API created

--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
 
-const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("database connected successfully");
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    message: "Welcome to the TineyTours",
+  });
+});
 
-}
+app.get("/health", (req, res) => {
+  res.json({
+    status: true,
+    message: "Server is Healthy",
+  });
+});
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log("database connected successfully");
+};
 
 app.listen("PORT", () => {
-    console.log(`Server is running on the ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on the ${PORT}`);
+  connectDB();
+});
